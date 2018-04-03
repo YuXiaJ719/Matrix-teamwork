bd52e87cc71c7b2db23018487ebca948ddf22d3f
zhlu2010 <zhlu2010@gmail.com>
2018-04-02 21:34:59 -0400

    operatormulitply

--- Matrix.cpp
+++ Matrix.cpp
@@ -1,1 +1,11 @@
-
+Matrix operator*(Matrix a, Matrix b) {
+    Matrix c(a.rows, b.cols);
+    for (int i = 0; i < c.rows; i++) {
+        for (int j = 0; j < c.rows; j++) {
+            for (int k = 0; k < a.cols; k++) {
+                c.mat[i * a.rows + j] += a.mat[i * a.cols + k] * b.mat[k * b.cols + j];
+            }
+        }
+    }
+    return c;
+}
